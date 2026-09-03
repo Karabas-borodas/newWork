b18f1e55cb7566e9a0b32b07bfe0dbdb47fcd815
MIhail <zoood3.14@gmail.com>
2026-09-03 07:43:54 +0300

    ADD: chanks

--- maxim/tasks/map/map.go
+++ maxim/tasks/map/map.go
@@ -2,7 +2,19 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
+
+var mas = map[string]int{
+	"d": 0}
+var ma = map[string]int{
+	"d": 4,
+	"b": 2,
+	"c": 3,
+	"a": 1,
+	"k": 5,
+}
+var empty = map[string]int{}
 
 // NOTE:1
 func CountInts(nums []int) map[int]int {
@@ -12,9 +24,97 @@ func CountInts(nums []int) map[int]int {
 	}
 	return ma
 }
+
+// NOTE:5
+func SortedKeys(m map[string]int) []string {
+	slice := make([]string, 0)
+	for i, _ := range m {
+		slice = append(slice, i)
+	}
+
+	sort.StringSlice(slice).Sort()
+	return slice
+}
+
+// NOTE:6
+func SumValues(m map[string]int) int {
+	summ := 0
+	for _, v := range m {
+		summ += v
+	}
+	return summ
+}
+
+// NOTE:7
+func Invert(m map[string]int) (map[int]string, error) {
+	ma := map[int]string{}
+	for i, v := range m {
+		if _, ok := ma[v]; ok {
+			return ma, fmt.Errorf("second element ")
+		} else {
+			ma[v] = i
+		}
+	}
+	return ma, nil
+}
+
+// NOTE:9
+func DeleteZeroValues(m map[string]int) {
+	for i, v := range m {
+		if v == 0 {
+			delete(m, i)
+		}
+	}
+}
+
+// NOTE:10
+func Clone(m map[string][]int) map[string][]int {
+	ma := make(map[string][]int)
+	for i, v := range m {
+		ma[i] = v
+	}
+	// ma["1"] = []int{5, 6}
+	return ma
+}
+
+// NOTE:12
+func Difference(a, b []int) []int {
+	ma := map[int]struct{}{}
+	for _, v := range b {
+		ma[v] = struct{}{}
+	}
+	slice := []int{}
+	for _, v := range a {
+		if _, ok := ma[v]; !ok {
+			slice = append(slice, v)
+		}
+	}
+	return slice
+}
+
 func main() {
 	slice := []int{1, 2, 3, 4, 5, 6, 2, 3, 1, 2}
 	fmt.Println(slice)
 	fmt.Println("#1")
 	fmt.Println(CountInts(slice))
+	fmt.Println("#5")
+	fmt.Println(SortedKeys(ma))
+	fmt.Println("#6")
+	fmt.Println(SumValues(ma))
+	fmt.Println(SumValues(empty))
+	fmt.Println("#7")
+	m, err := Invert(ma)
+	fmt.Println(m)
+	fmt.Println(err)
+	fmt.Println("#9")
+	DeleteZeroValues(ma)
+	fmt.Println(mas)
+	fmt.Println("#10")
+	maq := map[string][]int{"1": {1, 2, 3},
+		"2": {2, 3, 4}}
+	fmt.Println(Clone(maq))
+	fmt.Println(maq)
+	fmt.Println("#12")
+	b := []int{2, 5}
+	fmt.Println(Difference(slice, b))
 }
--- maxim/tasks/slice/7.go
+++ maxim/tasks/slice/7.go
@@ -101,8 +101,85 @@ func Unique(s []int) []int {
 	return slice
 }
 
-//NOTE:47 сделать
-//NOTE:49 сделать
+// NOTE:47 сделать
+func ChunkViews(s []int, size int) ([][]int, error) {
+	// if len(s) == 0 {
+	// 	return [][]int{}, nil
+	// }
+	// if size < 0 {
+	// 	return nil, fmt.Errorf("size cant be lou %d", size)
+	// }
+	//
+	// slice := make([][]int,  size)
+	// count := size
+	// lavelSlice := 0
+	// for i, v := range s {
+	//
+	// 	slice[lavelSlice] = append(slice[lavelSlice], v)
+	// 	if i == count {
+	// 		count += size + 1
+	// 		lavelSlice++
+	// 	}
+	// }
+	// return slice, nil
+
+	if len(s) == 0 {
+		return [][]int{}, nil
+	}
+	if size < 0 {
+		return nil, fmt.Errorf("size cant be lou %d", size)
+	}
+
+	countChank := (len(s) + size - 1) / size
+
+	slice := make([][]int, countChank)
+	count := size
+	lavelSlice := 0
+	var sl []int
+	slice[0] = sl
+	for i, v := range s {
+		slice[lavelSlice] = append(slice[lavelSlice], v)
+		if i == count {
+			count += size
+			lavelSlice++
+			var sl2 []int
+			slice[lavelSlice] = sl2
+			// slice[lavelSlice] = append(slice[lavelSlice], v)
+		}
+	}
+	return slice, nil
+}
+
+// NOTE:49 сделать
+func SplitIntoKViews(s []int, k int) ([][]int, error) {
+	if len(s) < 1 {
+		return [][]int{}, nil
+	}
+	if k < 1 {
+		return nil, fmt.Errorf("aaaaa all bad")
+	}
+	if k > len(s) {
+		k = len(s)
+	}
+	chankLangth := (len(s)) / k
+	chankLitlBit := len(s) % k
+	slice := make([][]int, k)
+	sl := make([]int, chankLangth+chankLitlBit)
+	sliceCount := 0
+	slice[sliceCount] = sl
+	start := 0
+	for i := 0; i < k; i++ {
+		size := chankLangth
+		if i < chankLitlBit {
+			size++
+		}
+		chank := s[start : size+start]
+		slice[i] = chank
+		start += size
+	}
+	return slice, nil
+}
+
 //NOTE:65 сделать
 
 func main() {
@@ -118,5 +195,11 @@ func main() {
 	fmt.Println("18")
 	(MoveZerosToEnd(slice))
 	MoveZerosToEnd([]int{0, 1, 0, 3, 0, 5, 8})
+	fmt.Println("29")
 	fmt.Println(Unique([]int{1, 1, 3, 4, 3, 3, 4, 4, 5, 5}))
+	fmt.Println("47")
+	fmt.Println(ChunkViews([]int{1, 1, 3, 4, 3, 3, 4, 4, 5, 5}, 3))
+	fmt.Println(ChunkViews([]int{1, 4, 2, 3}, 3))
+	fmt.Println("49")
+	fmt.Println(SplitIntoKViews([]int{1, 4, 2, 3}, 3))
 }
