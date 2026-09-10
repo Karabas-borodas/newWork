d9c8ffb20850bc278ef51a5c173a3f6b0a6e0993
MIhail <zoood3.14@gmail.com>
2026-09-10 18:45:51 +0300

    ADD: string 16

--- maxim/tasks/string/strings.go
+++ maxim/tasks/string/strings.go
@@ -138,13 +138,50 @@ func CountOverlapping(s, sub string) (int, error) {
 	}
 	return count, nil
 }
+
+// NOTE:16
+// Найдите самый длинный общий префикс всех строк в слайсе. Для пустого слайса
+// верните пустую строку.
+func LongestCommonPrefix(items []string) string {
+	if len(items) == 1 {
+		return items[0]
+	}
+	if len(items) == 0 {
+		return ""
+	}
+	var str = items[0]
+	for _, c := range items {
+		if len(str) > len(c) {
+			str = c
+		}
+	}
+	count := 0
+	for i := 1; i < len(items); i++ {
+		for j := 0; j < len(str); j++ {
+			if str[j] != items[i][j] {
+				break
+
+			} else {
+				count++
+
+			}
+		}
+		str = str[:count]
+		count = 0
+		// fmt.Println(len(str))
+		// str = count
+	}
+
+	return str
+}
+
 func main() {
-	var string = "Странные дела! Awada Kedawra \n OP musorok ne shey mne SroKK"
-	fmt.Println(string)
+	var stringers = "Странные дела! Awada Kedawra \n OP musorok ne shey mne SroKK"
+	fmt.Println(stringers)
 	fmt.Println("#1")
-	fmt.Println(StringSize(string))
+	fmt.Println(StringSize(stringers))
 	fmt.Println("#3")
-	fmt.Println(FirstRune(string))
+	fmt.Println(FirstRune(stringers))
 	fmt.Println("#8")
 	fmt.Println(IsPalindrome("ШаЛаШ"))
 	fmt.Println(IsPalindrome("аЛаШ"))
@@ -155,11 +192,15 @@ func main() {
 	fmt.Println(NormalizeSpaces("     "))
 	fmt.Println(NormalizeSpaces(" a b "))
 	fmt.Println("#12")
-	fmt.Println(IndexOf(string, "дела"))
+	fmt.Println(IndexOf(stringers, "дела"))
 	fmt.Println(IndexOf("ab", "x"))
 	fmt.Println("#14")
 	fmt.Println(CountOverlapping("aaaa", "aa"))
 	fmt.Println(CountOverlapping("aaaa", "aa"))
 	fmt.Println(CountOverlapping("aaaa", ""))
 	fmt.Println(CountOverlapping("aa  aa", "aa"))
+	fmt.Println("#16")
+	fmt.Println(LongestCommonPrefix([]string{"aaa", "aasss", "aaab"}))
+	fmt.Println(LongestCommonPrefix([]string{"", "aasss", "aaab"}))
+	fmt.Println(LongestCommonPrefix([]string{"abc", "aasss", "aaab"}))
 }
