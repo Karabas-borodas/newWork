89ccbacedd5426c14ffa98bac6b1fe5b3d7cced0
MIhail <zoood3.14@gmail.com>
2026-09-15 18:27:41 +0300

    ADD: struct 3

--- maxim/tasks/struct/main.go
+++ maxim/tasks/struct/main.go
@@ -6,9 +6,20 @@ import (
 
 func main() {
 
+	fmt.Println("-----1----")
 	var Id int = 1
 	var name string = "Aloha"
 	var age int = 10
 	user := NewUser(Id, name, age)
 	fmt.Println(user)
+	fmt.Println("-----2----")
+	var x = 45
+	var y = 67
+	point := NewPoint(x, y)
+	fmt.Printf("point %v", point)
+	fmt.Println("-----3----")
+	var r Rectangle
+	r.Heigth = 4
+	r.Width = 5
+	fmt.Println(Area(r))
 }
--- maxim/tasks/struct/struct.go
+++ maxim/tasks/struct/struct.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 // NOTE:1
@@ -24,4 +25,39 @@ func NewUser(id int, name string, age int) *User {
 	user.Age = age
 	user.Name = name
 	return &user
+}
+
+// NOTE:2
+// Определите структуру Point с координатами X int и Y int. Напишите функцию NewPoint(x, y int)
+// Point.
+type Point struct {
+	X int
+	Y int
+}
+
+func NewPoint(x, y int) *Point {
+	var p Point
+	p.X = x
+	p.Y = y
+	return &p
+}
+
+// NOTE:3
+// Определите структуру Rectangle с полями Width int и Height int. Напишите функцию Area(r
+// Rectangle) int.
+type Rectangle struct {
+	Width  int
+	Heigth int
+}
+
+func Area(x Rectangle) int {
+	if x.Width < 0 || x.Heigth < 0 {
+		return -1
+	}
+	if x.Heigth > 0 && x.Width > math.MaxInt/x.Heigth {
+
+		return -1
+	}
+	ar := x.Heigth * x.Width
+	return ar
 }
