cd29c59fcff2f2a961b38251256436fb1f1c995c
MIhail <zoood3.14@gmail.com>
2026-09-20 14:48:44 +0300

    ADD: tests struct 16

--- maxim/tasks/struct/struct_test.go
+++ maxim/tasks/struct/struct_test.go
@@ -28,20 +28,30 @@ func Test6_1(t *testing.T) {
 	}
 
 	user2 := WithEmail(user, "5553535")
-	if user.ID == user2.ID && user.Name == user2.Name && user.Age == user2.Age && user2.Email == "5553535" {
+	if user.ID != user2.ID || user.Name != user2.Name || user.Age != user2.Age || user2.Email != "5553535" {
 		t.Errorf("%d==0,%d==o", user.Age, user2.Age)
 	}
 }
+
 func Test6_2(t *testing.T) {
-	var user User = User{ID: 15,
-		Name: "ALah",
-		Age:  3,
-	}
-	var user2 = WithEmail(user, "5553535")
+	user := User{ID: 15, Name: "ALah", Age: 3}
+
+	user2 := WithEmail(user, "5553535")
 	user2.ID = 10
-	if user.ID != 15 && user2.ID != 10 {
-		t.Errorf("%d==0,%d==o", user.ID, user2.ID)
+
+	if user.ID != 15 {
+		t.Errorf("original mutated: user.ID = %d, want 15", user.ID)
 	}
-	fmt.Println(user)
-	fmt.Println(user2)
+	if user2.ID != 10 {
+		t.Errorf("copy not modified: user2.ID = %d, want 10", user2.ID)
+	}
+	if user2.Email != "5553535" {
+		t.Errorf("user2.Email = %q, want %q", user2.Email, "5553535")
+	}
+	if user.Email != "" {
+		t.Errorf("original Email mutated: %q", user.Email)
+		fmt.Printf("user  = %+v\n", user)
+		// fmt.Printf("user2 = %+v\n", user2)
+	}
+
 }
